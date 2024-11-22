c4b3ffd8b055222e26f0515ddd071f63c0d07177
X-osadmin <9164356+xosadmin@users.noreply.github.com>
2024-11-22 11:02:42 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,59 @@
+#!/bin/sh -e
+function checkASN() {
+  local retrieveASN=$1
+  if [ -z "$retrieveASN" ]; then
+    echo "The ASN number is not specified."
+    return 1
+  fi
+  if ! [[ "$retrieveASN" =~ ^[0-9]+$ ]]; then
+    echo "Invalid ASN number!"
+    return 1
+  fi
+  if [ "$retrieveASN" -lt 1 ] || [ "$retrieveASN" -gt 4294967295 ]; then
+    echo "Invalid ASN number!"
+    return 1
+  fi
+  if [ "$retrieveASN" -ge 0 ] && [ "$retrieveASN" -le 1023 ]; then
+        echo "Bogon ASN detected."
+        return 1
+    elif [ "$retrieveASN" -eq 23456 ]; then
+       echo "Bogon ASN detected."
+        return 1
+    elif [ "$retrieveASN" -ge 64512 ] && [ "$retrieveASN" -le 65535 ]; then
+        echo "Bogon ASN detected."
+        return 1
+    elif [ "$retrieveASN" -ge 4200000000 ] && [ "$retrieveASN" -le 4294967295 ]; then
+        echo "Bogon ASN detected."
+        return 1
+  fi
+  return 0
+}
+
+if [ ! -f /etc/bgpalerter/bgpalerter ]; then
+  echo "Cannot find bgpalerter, downloading..."
+  wget https://github.com/nttgin/BGPalerter/releases/latest/download/bgpalerter-linux-x64 -O /etc/bgpalerter/bgpalerter
+  chmod a+x /etc/bgpalerter/bgpalerter
+else
+  echo "bgpalerter found. Skip downloading."
+fi
+if [ ! -f /etc/bgpalerter/config.yml ]; then
+  echo "Cannot find config.yml, downloading default config..."
+  wget https://raw.githubusercontent.com/nttgin/BGPalerter/main/config.yml.example -O /etc/bgpalerter/config.yml
+else
+  echo "config.yml found. Skip downloading."
+fi
+
+checkASN "$ASN"
+checkASNNum=$?
+
+if [ checkASNNum -eq 1 ]; then
+  exit 1
+else
+  echo "Valid ASN $ASN detected, generating prefix list..."
+  cd /etc/bgpalerter
+  /etc/bgpalerter/bgpalerter generate -a $ASN -o prefixes.yml -i -m
+fi
+
+echo "All good for now. Starting bgpalerter..."
+cd /etc/bgpalerter
+/etc/bgpalerter/bgpalerter
